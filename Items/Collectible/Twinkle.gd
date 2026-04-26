e190278fc6fab30a886c6e04979f9bc2fbd06a2f
John Engel <johnengel@sourceharvest.com>
2026-04-25 21:06:44 -0400

    Improvements to the Twinkle algorithm.

--- Items/Collectible/Twinkle.gd
+++ Items/Collectible/Twinkle.gd
@@ -9,25 +9,25 @@ var time = 0.0
 var _player : Variant = null
 var _prev_player_position : Vector2
 
-func player_moving() -> bool:
-	if _player == null:
-		_player = get_tree().get_nodes_in_group("Player")[0]
-		_prev_player_position = _player.global_position
-	var camera = _player.camera
-	var viewport = camera.get_viewport()
-	var pos = camera.get_target_position() - (viewport.size/2.0)
-	var rect = Rect2(pos, viewport.size)
-	var in_viewport = (rect.has_point(global_position))
-	
-	var _player_moving = (_player.global_position != _prev_player_position)
+func _init_player():
+	_player = get_tree().get_nodes_in_group("Player")[0]
 	_prev_player_position = _player.global_position
-	return _player_moving and in_viewport
+	
+func set_shader_params():
+	var fn = FastNoiseLite.new()
+	fn.domain_warp_amplitude = 5.0
+	fn.frequency = 0.0001
+	var x = _player.global_position.x + global_position.x
+	var y = _player.global_position.y
+	var noise = fn.get_noise_2d(x, y)
+	material.set_shader_parameter("time", noise*25.0)
 	
 func _ready():
-	time += abs(global_position.x*0.0007)
-	material.set_shader_parameter("time", time)
+	_init_player()
+	set_shader_params()
+	#time += abs(global_position.x*0.0007)
+	set_shader_params()
 
 func _process(_delta):
-	if player_moving():
-		material.set_shader_parameter("time", time*2.5)
-		time += _delta
+	time += _delta
+	set_shader_params()
