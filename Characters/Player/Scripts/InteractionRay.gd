b8e77d6fe7368e5fbf5d7ac27228826f05c1fe67
John Engel <johnengel@sourceharvest.com>
2026-08-15 18:17:53 -0400

    Replaced 'interaction message' with 'interaction target.'

--- Characters/Player/Scripts/InteractionRay.gd
+++ Characters/Player/Scripts/InteractionRay.gd
@@ -1,25 +1,17 @@
 extends RayCast2D
 
-var message_showing = false
-var _message = load("res://UI/InteractionMessage.tscn")
-var message = null
-var temp_message = ""
+@onready var icon = $InteractionIcon
 var using_item : Variant = null
 var using_item_count = 1
+var positive_color : Color = Color(0.2,0.85,0.2)
+var negative_color : Color = Color(0.85,0.2,0.2)
+var _timer := 0.0
+var _blinking := false
+var _blink_duration := 0.5
+var _icon_state := false
 
-func show_message(text, _position):
-	message = _message.instantiate()
-	message.text = text
-	if not temp_message.is_empty():
-		message.text = temp_message
-	add_child(message)
-	
-func hide_message():
-	if message:
-		message.queue_free()
-
-func _process(_delta):
-	if is_colliding():
+func check_for_interactable():
+	if is_colliding(): 
 		var col = get_collider()
 		# IDk it randomly crashed once bc col was null. I think it's because it intersects with
 		# ONe of the enemy's projectiles right as the enemy's killed -- freeing all the projectiles.
@@ -27,32 +19,39 @@ func _process(_delta):
 			return
 		# TODO: Would col is Interactable work?
 		if col.is_in_group("Interactable"):
-			if not message_showing:
-				if "interaction_message" in col:
-					show_message(col.interaction_message, col.position)
-				else:
-					show_message("Z to interact", col.position)
-				message_showing = true
-			else:
-				if temp_message.is_empty():
-					if "interaction_message" in col:
-						message.text = col.interaction_message
+			_icon_state = true
+			if not get_parent().in_dialogue:
+				if "activate" in col:
+					if not using_item == null:
+						col.activate(using_item, using_item_count)
+					else:
+						col.activate()
+				using_item = null
 		else:
-			if message_showing:
-				hide_message()
-				message_showing = false
-		if message_showing:
-			if Input.is_action_just_pressed("Interact"):
-				if not get_parent().in_dialogue:
-					if "activate" in col:
-						if not using_item == null:
-							col.activate(using_item, using_item_count)
-						else:
-							col.activate()
-					temp_message = ""
-					using_item = null
+			_icon_state = false
 	else:
-		if message_showing:
-			hide_message()
-			message_showing = false
-			
+		_icon_state = false
+				
+func _process(_delta):
+	if Input.is_action_just_pressed("Interact"):
+		icon.position = target_position
+		check_for_interactable()
+		_blinking = true
+	if _blinking:
+		if _timer >= _blink_duration:
+			_timer = 0.0
+			_blinking = false
+		blink_icon(_icon_state)
+		_timer += _delta
+	else:
+		icon.modulate.a = 0.0
+		
+
+	
+func blink_icon(state : bool):
+	if state:
+		icon.modulate = positive_color
+	else:
+		icon.modulate = negative_color
+	var padink = Utils.padink(_timer, _blink_duration, 0.3, 1.5, 2.0, 0.0, 0.66)
+	icon.modulate.a = padink
